e71eee49f78ed7a8e3c52f2d27d9ac1f476abf49
kennethavendano <141265820+kennethavendano@users.noreply.github.com>
2023-08-04 09:19:13 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Kenneth Avendano'
+copyright = '2023, Kenneth Avendano'
+author = 'Kenneth Avednano'
 
 
 
